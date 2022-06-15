77f433301e10f878516815da879de9ec36b3e89f
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-06-15 20:03:24 +0300

    add upwork ad on the top

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
 import styled from "styled-components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+          <p>
+            Find talented pros in recort time with Upwork and keep business
+            moving
+          </p>
+        </h5>
+      </Section>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+
+const Content = styled.div``;
+
+const Section = styled.div``;
 
 export default Home;
