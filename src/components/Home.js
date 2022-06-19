2a4839c31b2cc0761b6d3e84413ec40f941cd1c2
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-06-19 23:01:18 +0300

    add UpWork ad on the top

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,10 +16,43 @@ const Home = () => {
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
 
-const Content = styled.div``;
+const Content = styled.div`
+  max-width: 1128px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
-const Section = styled.div``;
+const Section = styled.div`
+  min-height: 50px;
+  padding: 16px;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+
+  p {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
 
 export default Home;
