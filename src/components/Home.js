05e2b602750f695d04ec2230cd11a3da66938aeb
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-06-21 22:48:33 +0300

    create home screen section structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = () => {
   return (
@@ -12,6 +15,11 @@ const Home = () => {
           </p>
         </h5>
       </Section>
+      <Layout>
+        <Leftside />
+        <Main />
+        <Rightside />
+      </Layout>
     </Container>
   );
 };
@@ -50,6 +58,21 @@ const Section = styled.div`
   }
 
   @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftsides main rightsides";
+  grid-template-columns: minmax(0, 5fr), minmax(0, 12fr), minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-rows: auto; */
+  margin: 25px 0;
+  @media (min-width: 768px) {
+    display: flex;
     flex-direction: column;
     padding: 0 5px;
   }
---
+++ src/components/Rightside.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rightside = () => {
+  return <Container>rightside</Container>;
+};
+
+const Container = styled.div`
+  grid-area: Rightside;
+`;
+
+export default Rightside;
